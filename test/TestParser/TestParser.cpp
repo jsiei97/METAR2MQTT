dc0a548e55317d3c89a2385ee57f8734ac9b88e2
Johan Simonsson <github@fun-tech.se>
2012-09-22 00:17:14 +0200

    Parse the testdata so I can use it with the parser

--- test/TestParser/TestParser.cpp
+++ test/TestParser/TestParser.cpp
@@ -1,6 +1,12 @@
 #include <QtCore>
 #include <QtTest>
 
+#include <QFile>
+#include <QtXml>
+#include <QDomDocument>
+#include <QDomElement>
+#include <QDomNode>
+
 
 class TestParser : public QObject
 {
@@ -18,6 +24,7 @@ void TestParser::testParseFile_data()
 {
     QTest::addColumn<QString>("metarfile");
 
+    QTest::newRow("mini") << "mini.xml";
     QTest::newRow("EKCH") << "METAR_EKCH.xml";
     QTest::newRow("ESMK") << "METAR_ESMK.xml";
     QTest::newRow("ESMS") << "METAR_ESMS.xml";
@@ -27,14 +34,74 @@ void TestParser::testParseFile()
 {
     QFETCH(QString,metarfile);
     QFile file(metarfile);
-    
+
     if(!file.exists())
     {
         qDebug() << "Can't find testfile:" << metarfile << file.fileName();
+        QFAIL("No file");
     }
 
     //1. Open file
     //2. every row is a testcase
+
+
+    if (!file.open(QIODevice::ReadOnly))
+    {
+        qDebug() << "Can't open testfile:" << metarfile << file.fileName();
+        QFAIL("Readerror");
+    }
+
+    QDomDocument doc("configfile");
+    if (!doc.setContent(&file))
+    {
+        file.close();
+        qDebug() << "Can't parse testfile:" << metarfile << file.fileName();
+        QFAIL("Parse error");
+    }
+    file.close();
+
+    QDomElement docElem = doc.documentElement();
+
+    for(QDomNode n = docElem.firstChild(); !n.isNull(); n = n.nextSibling())
+    {
+        QDomElement e = n.toElement(); // try to convert the node to an element.
+        if(!e.isNull())
+        {
+            //qDebug() << e.tagName();
+            if(e.tagName() == "row")
+            {
+                QDomNodeList tagId = e.elementsByTagName("field");
+
+                /*
+                for(int i=0; i<tagId.count(); i++)
+                {
+                    QDomElement tagE = tagId.at(i).toElement();
+                    qDebug() << qPrintable(tagE.tagName()) << tagE.text();
+                }
+                */
+                QCOMPARE(tagId.count(), 5);
+
+                /*
+                qDebug() << tagId.at(0).toElement().text()
+                    << tagId.at(1).toElement().text()
+                    << tagId.at(2).toElement().text()
+                    << tagId.at(3).toElement().text()
+                    << tagId.at(4).toElement().text();
+                    */
+
+                // This is the test input for the parser
+                // 0 => date
+                // 1 => metar code aka input
+                // 2 => temperature
+                // 3 => dewpoint
+                // 4 => airpressure
+                
+
+
+            }
+
+        }
+    }
 
 
 }
